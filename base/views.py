c4e343ec03b36b456fa698cf5441aee8db76a040
jonasbarcelona2022 <barcelona.jonas09@gmail.com>
2022-06-11 16:13:56 +0800

    added templates and dynamic routes

--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
-
+rooms = [
+    
+        {'id': 1, 'name':'jonas '},
+        {'id': 2, 'name':'tine '},
+        {'id': 3, 'name':'jojo '},
+             
+]
 def home(request):
-    return HttpResponse("hello Django")
-def room(request):
-    return HttpResponse("welcome to the room")
+    # return HttpResponse("hello Django")
+    content = {'rooms':rooms}
+    return render(request, 'base/home.html',content)
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    content = {'room':room}
+    # return HttpResponse("welcome to the room")
+    return render(request, 'base/room.html',content)
